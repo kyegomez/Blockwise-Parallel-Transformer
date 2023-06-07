73800853fe7eed45ab3c9d28b6a0439de3ae1bbe
Kye <kye@apacmediasolutions.com>
2023-06-06 22:52:39 -0400

    clean up

--- blockwise_parallel/__init__.py
+++ blockwise_parallel/__init__.py
@@ -1,3 +1,3 @@
 # from blockwise_parallel.blockwise_paralle import BlockwiseParallelTransformerAttention
 # from blockwise_parallel.test1 import BlockwiseParallelTransformer/
-from blockwise_parallel.jax_v1 import BlockwiseParallelTransformerAttention
+from blockwise_parallel.blockwise_parallel_jax import BlockwiseParallelTransformerAttention
